7f49c102d64c325d51f0620657f41f1ea09261a5
briann020910 <febrianadiarakamjt@gmail.com>
2025-02-24 21:47:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
+// main.js
 
-onload = () => {
+window.onload = () => {
+  // Hapus kelas "not-loaded" setelah delay 1 detik
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+
+    // Ambil elemen dengan ID 'title'
     const titleElement = document.getElementById('title');
-    
+
+    if (!titleElement) {
+      console.error("Elemen dengan ID 'title' tidak ditemukan!");
+      return;
+    }
+
     // Teks pertama
     const firstText = 'I LOVE U';
     const firstTitles = firstText.split('');
@@ -14,25 +23,27 @@ onload = () => {
     const secondTitles = secondText.split('');
     let secondIndex = 0;
 
+    // Animasi untuk teks pertama
     function animateFirstText() {
       if (firstIndex < firstTitles.length) {
         titleElement.innerHTML += firstTitles[firstIndex];
         firstIndex++;
-        setTimeout(animateFirstText, 300);
+        setTimeout(animateFirstText, 300); // Delay 300ms antar karakter
       } else {
         // Setelah teks pertama selesai, hilangkan dan mulai teks kedua
         setTimeout(() => {
           titleElement.innerHTML = ''; // Kosongkan teks
           animateSecondText(); // Mulai animasi teks kedua
-        }, 1000); // Delay sebelum pindah ke teks kedua
+        }, 1000); // Delay 1 detik sebelum pindah ke teks kedua
       }
     }
 
+    // Animasi untuk teks kedua
     function animateSecondText() {
       if (secondIndex < secondTitles.length) {
         titleElement.innerHTML += secondTitles[secondIndex];
         secondIndex++;
-        setTimeout(animateSecondText, 300);
+        setTimeout(animateSecondText, 300); // Delay 300ms antar karakter
       }
     }
 
@@ -40,5 +51,5 @@ onload = () => {
     animateFirstText();
 
     clearTimeout(c);
-  }, 1000);
+  }, 1000); // Delay 1 detik sebelum mulai animasi
 };
