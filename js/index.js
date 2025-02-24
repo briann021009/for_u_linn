c569c04a470999733a796541babf0a13035c2d73
briann020910 <febrianadiarakamjt@gmail.com>
2025-02-24 07:47:37 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
-const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
-for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+// Manipulasi teks untuk <h1> (title)
+const title = document.querySelector('.title');
+const mainText = 'I Have Something Special For You'.split('');
+mainText.forEach((char) => {
+  if (char !== ' ') {
+    title.innerHTML += `<span>${char}</span>`;
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style="margin-right: 20px;"></span>`;
   }
-}
+});
 
-const textElements = document.querySelectorAll('.title span');
-textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+// Manipulasi teks untuk <p> (subtitle)
+const subtitle = document.querySelector('.subtitle');
+const subText = 'Click the button below to see it!'.split('');
+subText.forEach((char) => {
+  if (char !== ' ') {
+    subtitle.innerHTML += `<span>${char}</span>`;
+  } else {
+    subtitle.innerHTML += `<span style="margin-right: 20px;"></span>`;
+  }
+});
+
+// Animasi delay acak untuk semua elemen <span>
+const allSpans = document.querySelectorAll('.title span, .subtitle span');
+allSpans.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Delay acak antara 0 hingga 3 detik
   element.style.animationDelay = `${randomDelay}s`;
 });
