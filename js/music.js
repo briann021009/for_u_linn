0c633c72908e0071ecc367f582b03663d7d4a437
briann020910 <febrianadiarakamjt@gmail.com>
2025-02-24 20:17:37 +0700

    Update music.js

--- js/music.js
+++ js/music.js
@@ -8,7 +8,7 @@
 <body>
   <!-- Audio Player -->
   <audio autoplay>
-    <source src="https://github.com/briann020910/for_u_linn/raw/refs/heads/main/js/about%20you%20x%20about%20you_1739676105_musicaldown.com.mp3" type="audio/mp3">
+    <source src="https://github.com/briann020910/for_u_linn/raw/refs/heads/main/music/musik2.mp3" type="audio/mp3">
     Your browser does not support the audio element.
   </audio>
 </body>
