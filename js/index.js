ef6f78f5803db31803d872053b4e8fd8a4ca4642
briann020910 <febrianadiarakamjt@gmail.com>
2025-02-25 19:42:55 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Manipulasi teks untuk <h1> (title)
 const title = document.querySelector('.title');
-const mainText = 'Special For You‼️🫵'.split('');
+const mainText = 'For You My Love'.split('');
 mainText.forEach((char) => {
   if (char !== ' ') {
     title.innerHTML += `<span>${char}</span>`;
