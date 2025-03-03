1f7434e5d669631bb46f6a7e2c847a58d6271c11
briann020910 <febrianadiarakamjt@gmail.com>
2025-03-03 23:52:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ window.onload = () => {
         if (charIndex < currentTitles.length) {
           titleElement.innerHTML += currentTitles[charIndex];
           charIndex++;
-          setTimeout(displayCharacter, 87); // Ubah delay menjadi 87ms untuk kecepatan lebih tinggi
+          setTimeout(displayCharacter, 150); // Ubah delay menjadi 87ms untuk kecepatan lebih tinggi
         } else {
           // Setelah teks selesai, kosongkan dan pindah ke teks berikutnya
           setTimeout(() => {
